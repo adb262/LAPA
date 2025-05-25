b9e6208dadc0559ff2d1d3f3bcf819867f1af522
adb2622 <adb262@cornell.edu>
2025-05-25 10:15:44 -0700

    Make installable

---
+++ setup.py
@@ -0,0 +1,14 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='lapa-form',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        line.strip()
+        for line in open('requirements.txt').readlines()
+        if line.strip() and not line.startswith('#')
+    ],
+)
